9dac24a1646145f29a4b54ff31e64827670963db
Langery <1245066197@qq.com>
2019-05-14 20:48:27 +0800

    router

--- Express/myapp/routes/index.js
+++ Express/myapp/routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router
+  .get('/', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+  })
+  .get('/other', function(req, res, next) {
+    // res.send('respond with a resource');
+    res.render('layout', { title: 'demo' })
+  })
 
 module.exports = router;
